9e680552750ccc6b20411e544c02df13ea4bbba1
daniel-orlov <daniel.orlov@ctemplar.com>
2020-05-28 17:51:40 +0200

    adding ex.70, 71 + sampleForReading.txt (will wait w/ 69)

---
+++ exercises/071.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+Using label to mark the chunk of code as Loop
+We can also control the code flow using goto Loop
+ */
+
+import "fmt"
+
+func main()  {
+	x := 12
+
+InfNestedLoop:
+	for{
+		for {
+			x++
+			fmt.Println(x)
+			switch x {
+			case 21:
+				fmt.Println("Target value reached, breaking")
+				break InfNestedLoop //breaking the whole lable InfNestedLoop
+			}
+		}
+	}
+
+}
