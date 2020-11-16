8a1fc3b87ce532700e42d3b4fac9ecd505e554d7
daniel-orlov <daniel.orlov@ctemplar.com>
2020-11-16 18:11:43 +0300

    commiting earlier files

---
+++ first-router.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/test", TestHandler)
+	http.Handle("/", router)
+	fmt.Println("All set up")
+}
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ stat-dyn-server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func main() {
+	http.HandleFunc("/static", serveStaticWebpage)	//endpoint "/static"
+	http.HandleFunc("/", serveTimeDynamically)		//endpoint "/"
+	http.ListenAndServe(PORT, nil)
+}
+
+func serveTimeDynamically(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ENDPOINT HIT: /")
+	response := fmt.Sprintf("Time is %v", time.Now().String())
+	fmt.Fprintln(w, response)
+}
+
+func serveStaticWebpage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ENDPOINT HIT: /static")
+	http.ServeFile(w, r, "res/html/static.html")
+}
