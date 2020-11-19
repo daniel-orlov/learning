6aa104b9670324d733de9d96837507d36ade59ba
daniel-orlov <daniel.orlov@ctemplar.com>
2020-11-19 21:38:46 +0300

    refactoring interfaces.go adding new file map_to_pointers.go

--- interfaces.go
+++ interfaces.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+const (
+	DOUBLE = "====================================================================================================="
+	SINGLE = "-----------------------------------------------------------------------------------------------------"
+)
+
 type Struct struct{}
 
 func (strukt Struct) Function() {}
@@ -38,39 +43,27 @@ func InitIfacePointer() Interface {
 func main() {
 
 	fmt.Println("        \tVALUE\t\tGO-SYNTAX REPRESENTATION\t\tTYPE\t\t\t==nil")
-	fmt.Println(
-		"=====================================================================================================",
-	)
+	fmt.Println(DOUBLE)
 	s := InitType()
 	fmt.Printf("    Struct\t%v\t\t%#v\t\t\t\t%T\t\t%v\n", s, s, s, "ERROR")
 	fmt.Printf(" ^pointer^\t%v\t\t%#v\t\t\t\t%T\t\t%v\n", &s, &s, &s, &s == nil)
-	fmt.Println(
-		"-----------------------------------------------------------------------------------------------------",
-	)
+	fmt.Println(SINGLE)
 	ps := InitPointer() //ps != &s
 	fmt.Printf("   *Struct\t%v\t\t%#v\t\t\t%T\t\t%v\n", ps, ps, ps, ps == nil)
 	fmt.Printf(" ^pointer^\t%v\t%#v\t\t%T\t\t%v\n", &ps, &ps, &ps, &ps == nil)
-	fmt.Println(
-		"-----------------------------------------------------------------------------------------------------",
-	)
+	fmt.Println(SINGLE)
 	e := InitEfaceType()
 	fmt.Printf(" (S)inface\t%v\t\t%#v\t\t\t\t%T\t\t%v\n", e, e, e, e == nil)
 	fmt.Printf(" ^pointer^\t%v\t%#v\t\t%T\t\t%v\n", &e, &e, &e, &e == nil)
-	fmt.Println(
-		"-----------------------------------------------------------------------------------------------------",
-	)
+	fmt.Println(SINGLE)
 	ep := InitEfacePointer()
 	fmt.Printf("(*S)inface\t%v\t\t%#v\t\t\t%T\t\t%v\n", ep, ep, ep, ep == nil)
 	fmt.Printf(" ^pointer^\t%v\t%#v\t\t%T\t\t%v\n", &ep, &ep, &ep, &ep == nil)
-	fmt.Println(
-		"-----------------------------------------------------------------------------------------------------",
-	)
+	fmt.Println(SINGLE)
 	i := InitIfaceType()
 	fmt.Printf(" (S)Interf\t%v\t\t%#v\t\t\t\t%T\t\t%v\n", i, i, i, i == nil)
 	fmt.Printf(" ^pointer^\t%v\t%#v\t\t%T\t\t%v\n", &i, &i, &i, &i == nil)
-	fmt.Println(
-		"-----------------------------------------------------------------------------------------------------",
-	)
+	fmt.Println(SINGLE)
 	ip := InitIfacePointer()
 	fmt.Printf("(*S)Interf\t%v\t\t%#v\t\t\t%T\t\t%v\n", ip, ip, ip, ip == nil)
 	fmt.Printf(" ^pointer^\t%v\t%#v\t\t%T\t\t%v\n", &ip, &ip, &ip, &ip == nil)
