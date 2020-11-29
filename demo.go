3a419ddcac7904be56b29eea6c35b409614787b7
daniel-orlov <daniel.orlov@ctemplar.com>
2020-11-29 20:23:59 +0300

    tail-recursion added actual demo.go appeared

---
+++ demo.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func demoBinarySearch() {
+	binArr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	sought := 5
+	index := BinarySearch(binArr, sought)
+	fmt.Println("BINARY SEARCH:\nArray: ", binArr)
+	fmt.Printf("Value '%v' found in arr at index %v\n", sought, index)
+}
+
+func demoSelectionSort() {
+	selSortArr := []int{3, 1, 4, 5, 9, 2, 6, 8, 7, 0}
+	fmt.Println("\nSELECTION SORT:\nArray: ", selSortArr)
+	sortedArr := SelectionSort(selSortArr)
+	fmt.Println("Sorted array: ", sortedArr)
+}
+
+func demoRecusionFactorial() {
+	fmt.Println("\nRECURSIVE FACTORIAL CALCULATION:")
+	x := 7
+	result := factorial(x, "|\t")
+	fmt.Printf("RESULT: Factorial %v = %v\n", x, result)
+}
+
+func demoRecursionSumArr() {
+	fmt.Println("\nRECURSIVE SUM CALCULATION:")
+	arr := []int{5, 7, 9, 12, 21}
+	result := sum(arr, "|\t")
+	fmt.Printf("RESULT: Sum of elements in an array %v = %v\n", arr, result)
+}
+
+func demoTailRecursionFactorial() {
+	fmt.Println("\nTAIL-RECURSIVE FACTORIAL CALCULATION:")
+	x := 7
+	result := tailFactorial(x, "|\t")
+	fmt.Printf("RESULT: Factorial %v = %v\n", x, result)
+}
+
+func demoQuickSort() {
+	quickSortArr := []int{3, 1, 4, 5, 9, 2, 6, 8, 7, 0}
+	fmt.Println("\nQUICK SORT:\nArray: ", quickSortArr)
+	quickSortedArr := QuickSort(quickSortArr)
+	fmt.Println("Sorted array: ", quickSortedArr)
+}
--- main.go
+++ main.go
@@ -1,36 +1,21 @@
 package main
 
-import "fmt"
-
 func main() {
 	//Binary Search
-	binArr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	sought := 5
-	index := BinarySearch(binArr, sought)
-	fmt.Println("BINARY SEARCH:\nArray: ", binArr)
-	fmt.Printf("Value '%v' found in arr at index %v\n", sought, index)
+	//demoBinarySearch()
 
 	//SelectionSort
-	selSortArr := []int{3, 1, 4, 5, 9, 2, 6, 8, 7, 0}
-	fmt.Println("\nSELECTION SORT:\nArray: ", selSortArr)
-	sortedArr := SelectionSort(selSortArr)
-	fmt.Println("Sorted array: ", sortedArr)
+	//demoSelectionSort()
 
 	//Recursion - Factorial
-	fmt.Println("\nRECURSIVE FACTORIAL CALCULATION:")
-	x := 5
-	result := factorial(x, "|\t")
-	fmt.Printf("RESULT: Factorial %v = %v\n", x, result)
+	demoRecusionFactorial()
+
+	//Tail-Recursion - Factorial
+	demoTailRecursionFactorial()
 
 	//Recursion - Sum of elements in an array
-	fmt.Println("\nRECURSIVE SUM CALCULATION:")
-	arr := []int{5, 7, 9, 12, 21}
-	result = sum(arr, "|\t")
-	fmt.Printf("RESULT: Sum of elements in an array %v = %v\n", arr, result)
+	//demoRecursionSumArr()
 
 	//QuickSort
-	quickSortArr := []int{3, 1, 4, 5, 9, 2, 6, 8, 7, 0}
-	fmt.Println("\nQUICK SORT:\nArray: ", quickSortArr)
-	quickSortedArr := QuickSort(quickSortArr)
-	fmt.Println("Sorted array: ", quickSortedArr)
+	//demoQuickSort()
 }
