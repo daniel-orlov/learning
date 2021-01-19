28c0af5e0e024d5ad7f661798ce1e912426ed80c
daniel-orlov <daniel.orlov@ctemplar.com>
2021-01-19 10:04:53 -0800

    better draft of DFS

--- search/depth_first_search.go
+++ search/depth_first_search.go
@@ -2,6 +2,37 @@ package search
 
 import (
 	"github.com/daniel-orlov/go-data-structures/graph"
+	ll "github.com/daniel-orlov/go-data-structures/linked"
+	"github.com/daniel-orlov/go-data-structures/stack"
 )
 
-func (g *graph.Graph) DepthFirstSearch(vertexID int)
+/*procedure DFS_iterative(G, v) is
+  let S be a stack
+  S.push(v)
+  while S is not empty do
+      v = S.pop()
+      if v is not labeled as discovered then
+          label v as discovered
+          for all edges from v to w in G.adjacentEdges(v) do
+              S.push(w)
+*/
+
+func (g *graph.Graph) DepthFirstSearch(vertexID int) {
+	zeroth := ll.NewElem(0, g.Vertices[0]) // this is not gonna work, gotta fix NewElem to work with ID first
+	dfsStack := stack.NewStack("dfs", zeroth)
+
+	discovered := make(map[int]bool) // TODO: Add size of Graph here once g.Size() implemented
+
+	for !dfsStack.IsEmpty() {
+		el := dfsStack.Pop()
+		if !discovered[el.ID] {
+			discovered[el.ID] = true
+			vtx := el.Data
+			neighbours := vtx.Vertices
+			for k, v := range neighbours {
+				vertex := ll.NewElem(k, v) // same as the 1st line of this func
+				dfsStack.Push(vertex)
+			}
+		}
+	}
+}
