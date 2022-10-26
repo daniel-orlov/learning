096f893406458b8f46a50ed958871918cdd4ddce
Daniel Orlov <daniel.orlov@protonmail.com>
2022-10-26 18:47:27 +0200

    feat: hello, dart, v4

--- part1/ch2/hello.dart
+++ part1/ch2/hello.dart
@@ -1,7 +1,12 @@
 void main() {
-  hello("World");
+  List<String> namesList = ['Seth', 'Kathy', 'Lars'];
+  print(namesList);
+
+  for (String name in namesList) {
+    hello(name);
+  }
 }
 
 void hello(String name) {
-  print('Hello, $name!');
+  print("Hello, $name!");
 }
